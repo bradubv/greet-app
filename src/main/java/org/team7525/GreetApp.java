dc44532cc652cfc4ea5b21cb6e4ebb31dc203061
Bogdan V. Bradu <bradubv@hotmail.com>
2024-12-09 23:45:43 -0600

    first commit

---
+++ src/main/java/org/team7525/GreetApp.java
@@ -0,0 +1,10 @@
+package org.team7525;
+
+//import org.team7525.GreetLibrary;
+
+public class GreetApp {
+  public static void main(String[] args) {
+    String message = GreetLibrary.greet("World");
+    System.out.println(message);
+  }
+}
